e02d90a2ba4a82686ee4a316aabecaa9c6a0ca95
Carlos Alberto <168146396+CarlinhosAlves@users.noreply.github.com>
2026-05-21 10:13:47 -0300

    Update numero_por_extenso.py

--- tuplas/numero_por_extenso.py
+++ tuplas/numero_por_extenso.py
@@ -1,6 +1,6 @@
 extenso  = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete',
         'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze','quinze', 'dezesseis',
-        'dezessete','dezoito', 'dezenove', 'vinte')
+        'dezessete','dezoito', 'dezenove', 'vinte') 
 
 num = int(input("Digite um número (entre 0 e 20): "))
 while num < 0 or num > 20:
