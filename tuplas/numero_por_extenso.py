679d3cb53582f487091c2a298081ad112d444826
Carlos Alberto <168146396+CarlinhosAlves@users.noreply.github.com>
2026-05-21 10:17:10 -0300

    Delete tuplas/numero_por_extenso.py

--- tuplas/numero_por_extenso.py
+++
@@ -1,9 +0,0 @@
-extenso  = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete',
-        'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze','quinze', 'dezesseis',
-        'dezessete','dezoito', 'dezenove', 'vinte') 
-
-num = int(input("Digite um número (entre 0 e 20): "))
-while num < 0 or num > 20:
-    print("Número inválido , tente novamente !")
-    num = int(input("Digite um número (entre 0 e 20): "))
-print(f"Você Digitou o número {extenso[num]}")
